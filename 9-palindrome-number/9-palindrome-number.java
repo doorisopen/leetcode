fb5276a0de27c52f74f19ca8e3e6e42f36dd9ec3
TaeWoong Lee <doorwasopen@gmail.com>
2022-03-12 14:11:42 +0900

    Time: 13 ms (63.82%), Space: 45 MB (28.31%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
         String inputStr = String.valueOf(x);
-        StringBuilder sb = new StringBuilder();
-        sb.append(inputStr);
-        sb.reverse();
-        return inputStr.equals(sb.toString());
+        int startIndex = 0;
+        int endIndex = inputStr.length() - 1;
+        while (startIndex < endIndex) {
+            if (inputStr.charAt(startIndex++) != inputStr.charAt(endIndex--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
