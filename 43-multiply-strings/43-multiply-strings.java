712b98890fd712c240d2896dd6af64f990df7aff
TaeWoong Lee <doorwasopen@gmail.com>
2022-08-17 21:58:40 +0900

    Time: 8 ms (63.27%), Space: 42.9 MB (67.98%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.length() < num2.length()) {
+            String temp = num2;
+            num2 = num1;
+            num1 = temp;
+        }
+        int[] calc = new int[num1.length() + num2.length()];
+        for (int i = num2.length() - 1; i >= 0; i--) {
+            for (int j = num1.length() - 1; j >= 0; j--) {
+                int mul = calc[i + j + 1] + (num1.charAt(j) - '0') * (num2.charAt(i) - '0');
+                calc[i + j] += (mul / 10);
+                calc[i + j + 1] = (mul % 10);
+            }
+        }
+        StringBuilder answer = new StringBuilder();
+        for (int val : calc) {
+            if (!(answer.length() == 0 && val == 0)) {
+                answer.append(val);
+            }
+        }
+        return answer.toString().length() == 0 ? "0" : answer.toString();
+    }
+}
