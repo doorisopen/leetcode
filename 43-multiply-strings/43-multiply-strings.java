7b466009f012389449fde0f2a3c82aebc11d64d2
TaeWoong Lee <doorwasopen@gmail.com>
2022-08-17 21:58:51 +0900

    Time: 7 ms (71.09%), Space: 43 MB (55.43%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,10 +1,5 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.length() < num2.length()) {
-            String temp = num2;
-            num2 = num1;
-            num1 = temp;
-        }
         int[] calc = new int[num1.length() + num2.length()];
         for (int i = num2.length() - 1; i >= 0; i--) {
             for (int j = num1.length() - 1; j >= 0; j--) {
